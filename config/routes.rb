c78479135b8d0cb4de16363f580846d48ba99fe1
antarr <antarr@hey.com>
2021-01-10 00:05:48 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -109,10 +109,10 @@ Rails.application.routes.draw do
   # to cuttlefish via the graphql api) and there is no immediate super easy
   # way to do authentication and check that the user is a site_admin.
   # So, disabling it for the moment. Let's hope we don't need it in an emergency
-  # require "sidekiq/web"
-  # authenticate :admin, ->(u) { u.site_admin? } do
-  #   mount Sidekiq::Web => "/sidekiq"
-  # end
+  require "sidekiq/web"
+  authenticate :admin, ->(u) { u.site_admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 
   resources :admins, only: %i[index destroy]
   resources :emails, only: %i[index show], as: :deliveries,
